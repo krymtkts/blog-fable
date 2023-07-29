202f88e5ebd2289caab16a3f5216e2f7981e36b3
krymtkts <takatoshi.kuriyama@gmail.com>
2023-07-29 18:20:29 +0900

    Remove parens.

--- src/Dev.fs
+++ src/Dev.fs
@@ -3,8 +3,8 @@ module Dev
 open Browser.Dom
 open Browser.WebSocket
 
-let private init (_) =
+let private init _ =
     let ws = WebSocket.Create $"ws://{window.location.host}/websocket"
-    ws.onmessage <- fun (_) -> window.location.reload ()
+    ws.onmessage <- fun _ -> window.location.reload ()
 
 window.addEventListener ("load", init)
